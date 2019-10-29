0fd74bb03475bc3325a1313ead30fb18582d34b3
soroush <soroush_safarii@yahoo.com>
2019-10-29 20:44:11 +0330

    Auto stash before rebase of "refs/heads/master"

--- camo/urls.py
+++ camo/urls.py
@@ -13,14 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('api/v1/user/', include('users.urls')),
-	path('api/v1/login/', views.obtain_auth_token),  # user can obtain token by giving username and pass
+	path('api/v1/token/', views.obtain_auth_token),  # user can obtain token by giving username and pass
 	path('api/v1/wallet/', include('wallet.urls')),
 	path('api/v1/report/', include('report.urls')),
+
+	# documentation
+	path('docs/', TemplateView.as_view(
+		template_name='redoc/index.html',
+		extra_context={'schema_url': 'redoc/schema.yml'}
+	), name='redoc'),
 ]
--- wallet/urls.py
+++ wallet/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SubmitIncome, SubmitExpense
 
 urlpatterns = [
-	path('income/', SubmitIncome.as_view()),
-	path('expense/', SubmitExpense.as_view()),
+	path('income/', SubmitIncome.as_view(), name='submit_income'),
+	path('expense/', SubmitExpense.as_view(), name='submit_expense'),
 ]
